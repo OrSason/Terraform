16b9a4b6a704f433c967d0c9a614252555fc88a2
Legans09 <72452018+Legans09@users.noreply.github.com>
2021-04-13 08:52:00 +0300

    main

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+
+resource "azurerm_virtual_network" "rg" {
+  name                = "virtualNetwork1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  
+  subnet {
+    name           = "Public"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "Private"
+    address_prefix = "10.0.2.0/24"
+  }
+
+}
+
