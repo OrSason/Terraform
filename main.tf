32583454171b61e7636bc94694716e27e4b0b7f9
Legans09 <72452018+Legans09@users.noreply.github.com>
2021-04-18 16:08:47 +0300

    mUpdates

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+#this script contains the providers and creating resource group
+
 terraform {
   required_providers {
     azurerm = {
@@ -12,28 +14,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
+  name     = var.resourceGroupName
+  location = var.location
 }
 
 
-resource "azurerm_virtual_network" "rg" {
-  name                = "virtualNetwork1"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  
-  subnet {
-    name           = "Public"
-    address_prefix = "10.0.1.0/24"
-  }
 
-  subnet {
-    name           = "Private"
-    address_prefix = "10.0.2.0/24"
-  }
 
-}
 
--- network.tf
+++ network.tf
@@ -1,24 +1,102 @@
-
-#Public load balancer
-
-resource "azurerm_public_ip" "LBPublicIP" {
-  name                = "PublicIPForLB"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
+#this script configure the virtual network,subnets and NSGs
+resource "azurerm_virtual_network" "vNet" {
+  name                = "wta-vnet"
+  location            = var.location
+  resource_group_name = var.resourceGroupName
+  address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_lb" "PublicLB" {
-  name                = "PublicLoadBalancer"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  
+resource "azurerm_subnet" "PublicSN" {
+  name                 = "public-subnet"
+  resource_group_name  = var.resourceGroupName
+  virtual_network_name = azurerm_virtual_network.vNet.name
+  address_prefixes     = ["10.0.1.0/24"]
 
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.LBPublicIP.id
-    subnet_id           = azurerm_subnet.PublicSubnet.id
+}
+
+resource "azurerm_subnet" "PrivateSN" {
+  name                 = "private-subnet"
+    resource_group_name  = var.resourceGroupName
+  virtual_network_name = azurerm_virtual_network.vNet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+}
+
+
+resource "azurerm_network_security_group" "public_nsg" {
+  name                = "public-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "8080"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "rule_22"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  
 }
 
+
+
+resource "azurerm_network_security_group" "private_nsg" {
+  name                = "private-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "5432_rule"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "5432"
+    destination_port_range     = "5432"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "22_ruleO"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "22"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  
+}
+
+
+
+resource "azurerm_subnet_network_security_group_association" "public_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.PublicSN.id
+  network_security_group_id = azurerm_network_security_group.public_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "private_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.PrivateSN.id
+  network_security_group_id = azurerm_network_security_group.private_nsg.id
+}
 
